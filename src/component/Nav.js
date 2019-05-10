9190d0fc9706ea15ff2f70c7880bee709eb4bffe
KarenQuezadaMejia <quezadakaren16@gmail.com>
2019-05-10 16:13:05 -0400

    inicio para estructura de nav

---
+++ src/component/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Nav.css';
+
+
+function Nav() {
+    return (
+      <div className="Nav">
+        <header className="Nav-header">
+          <p>
+            lorem
+          </p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Nav;
+  
