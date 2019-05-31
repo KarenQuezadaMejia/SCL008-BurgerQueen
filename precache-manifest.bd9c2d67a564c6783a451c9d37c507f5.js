ea6b119288cbcca65a396ea1d0cf63e24d76d1fd
KarenQuezadaMejia <quezadakaren16@gmail.com>
2019-05-31 14:35:20 -0400

    Updates

---
+++ precache-manifest.bd9c2d67a564c6783a451c9d37c507f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cced6ba9d6c39d3ec0d30c7918264bd",
+    "url": "/SCLO008-BurguerQueen/index.html"
+  },
+  {
+    "revision": "29f2493ef52fc82575b7",
+    "url": "/SCLO008-BurguerQueen/static/css/main.1eb3511d.chunk.css"
+  },
+  {
+    "revision": "202f8a8c0e18c9b0e45a",
+    "url": "/SCLO008-BurguerQueen/static/js/2.6b651a31.chunk.js"
+  },
+  {
+    "revision": "29f2493ef52fc82575b7",
+    "url": "/SCLO008-BurguerQueen/static/js/main.52f70336.chunk.js"
+  },
+  {
+    "revision": "c3f0da966fcd53006782",
+    "url": "/SCLO008-BurguerQueen/static/js/runtime~main.79e1c676.js"
+  },
+  {
+    "revision": "9f59266a37e1ab75a1d96e7fb2f951d2",
+    "url": "/SCLO008-BurguerQueen/static/media/logo.9f59266a.png"
+  }
+]);
